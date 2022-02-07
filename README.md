16e2c43f70e8c9933a6bf69dbbee2b216d3e9b19
rishabhmthakur2 <rishabhmthakur@outlook.com>
2022-02-07 13:44:01 -0800

    cats server

---
+++ README.md
@@ -0,0 +1,62 @@
+# Repo for a CRUD server to Create, Read, Update and Delete cats as resources
+# Done as part of a take-home assignment for DFinity Foundation
+
+- [Heroku Server Address] - 
+
+## You can also use local installation in place of the HEROKU endpoint
+
+1. Clone repo
+2. run `npm install` 
+
+## Usage 
+
+1. run `npm run dev` or `npm run start`
+2. You can now interact with the APIs on localhost:8000 using tools like POSTMAN
+
+## Data storage
+The cat objects are stored in a MongoDB database hosted on the cloud using MongoDB Atlas.
+
+## Endpoints
+
+1. POST /cats
+
+Input:
+Takes in a JSON body of the format below
+{
+    "catName": "testName",
+    "catColor": "testColor",
+    "catBreed": "testBreed"
+}
+
+Output:
+JSON response with a unique id for the cat
+
+2. GET /cats
+
+Input: None
+Output: List of all cat objects in the database
+
+3. GET /cats/:id
+
+Input: Unique Object Id for the cat as part of the URL param
+Output: JSON body with information regarding the cat
+
+4. PUT /cats/:id
+
+Input: 
+Unique Object Id for the cat as part of the URL param
+Takes in a JSON request body similar to POST call
+{
+    "catName": "updatedTestName",
+    "catColor": "testColor",
+    "catBreed": "testBreed"
+}
+Output: JSON body with updated information regarding the cat
+
+5. DELETE /cats/:id
+
+Input: Unique Object Id for the cat as part of the URL param
+Output: None(200 status code)
+
+## POSTMAN Collection
+There is a POSTMAN collection included in this repo which can be used for interacting with the HEROKU server. You will have to change the URL if you are running a local server.
---
+++ server.js
@@ -0,0 +1,122 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+var mongo = require("mongodb");
+
+var db;
+
+MongoClient.connect(
+  "mongodb+srv://dfinity:dfinity@cluster0.xtexu.mongodb.net/dfinity-cats?retryWrites=true&w=majority",
+  (err, database) => {
+    if (err) return console.log(err);
+    db = database.db("dfinity-cats");
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("listening on 8000");
+    });
+  }
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/cats", (req, res) => {
+  db.collection("cats")
+    .find()
+    .toArray((err, result) => {
+      if (err) return res.status(400).send(err);
+      res.status(200);
+      res.json({ cats: result });
+    });
+});
+
+app.get("/cats/:id", async (req, res) => {
+  try {
+    let objectId = new mongo.ObjectID(req.params.id);
+    const result = await db.collection("cats").findOne({ _id: objectId });
+    if (result != null) {
+      res.status(200);
+      res.json({
+        result,
+      });
+    } else {
+      throw new Error("Invalid ID");
+    }
+  } catch (e) {
+    res.status(400);
+    res.json({
+      status: 400,
+      error: "Invalid ID",
+    });
+  }
+});
+
+app.post("/cats", async (req, res) => {
+  const { catName, catColor, catBreed } = req.body;
+  if (
+    catName === undefined ||
+    catColor === undefined ||
+    catBreed == undefined
+  ) {
+    res.status(400);
+    res.json({ status: 400, error: "Invalid JSON body" });
+  } else {
+    const cat = {
+      catName,
+      catColor,
+      catBreed,
+    };
+    const result = await db.collection("cats").insertOne(cat);
+    res.status(201);
+    res.json({
+      status: 201,
+      message: `Cat inserted into record successfully with id: ${result.insertedId}`,
+      cat: { id: result.insertedId, catName, catBreed, catColor },
+    });
+  }
+});
+
+app.put("/cats/:id", (req, res) => {
+  let objectId = new mongo.ObjectID(req.params.id);
+  db.collection("cats").findOneAndUpdate(
+    { _id: objectId },
+    {
+      $set: {
+        catName: req.body.catName,
+        catColor: req.body.catColor,
+        catBreed: req.body.catBreed,
+      },
+    },
+    (err, result) => {
+      if (err) return res.send(err);
+      res.status(200);
+      res.json({
+        status: 200,
+        cat: {
+          catName: req.body.catName,
+          catColor: req.body.catColor,
+          catBreed: req.body.catBreed,
+        },
+      });
+    }
+  );
+});
+
+app.delete('/cats/:id', (req, res) => {
+  let objectId = new mongo.ObjectID(req.params.id);
+  db.collection('cats').findOneAndDelete({_id: objectId}, (err, result) => {
+    if (err || result.value == null) {
+      res.status(400)
+      res.json({
+        status: 400,
+        error: err ? err : 'Invalid ID' 
+      })
+    }
+    else{
+      res.status(200)
+      res.json({
+        status: 200
+      })
+    }
+  })
+})
